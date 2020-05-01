b6198114b3f58aa2b2abe9919a76576c2ff10cfc
hpkishere <hendrohpk@gmail.com>
2020-05-01 14:14:16 +0800

    Implement endpoint to get total confirmed cases by country

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, request, jsonify
+import requests
+import json
+app = Flask(__name__)
+
+BASE_URL = "https://api.covid19api.com"
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+# /api/cases/total?country_code=SG
+@app.route('/api/cases/total')
+def get_total_cases_by_country_code():
+
+    country_code = request.args.get('country_code')
+    get_url = BASE_URL + "/total/country/" + country_code
+
+    response = requests.get(get_url)
+
+    cases_list = response.json()[-1]
+
+    if response.ok:
+        return jsonify(cases_list)
+    else:
+        return jsonify({
+            "message": "Something went wrong"
+        })
